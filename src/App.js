e4b4da58df28498b1aa8c4afbcebc865831b19ed
Latemiq <dzierzkowskij@gmail.com>
2023-03-19 15:04:02 +0100

    ADD: icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Sidebar from './Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       {/* Sidebar */}
-
+        <Sidebar />
     {/* Feed */}
 
     {/* Widgets */}
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import * as React from 'react';
+import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import HomeIcon from '@mui/icons-material/Home';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import EmailIcon from '@mui/icons-material/Email';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 function Sidebar() {
   return (
-    <div>
-      
+    <div className='sidebar'>
+      {/* Twitter icon */}
+      <TwitterIcon />
+      <SidebarOption text="Home" Icon={HomeIcon} />
+      <SidebarOption text="Explore" Icon={SearchIcon} />
+      <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
+      <SidebarOption text="Messages" Icon={EmailIcon} />
+      <SidebarOption text="Bookmarks" Icon={BookmarkIcon} />
+      <SidebarOption text="Lists" Icon={ListAltIcon} />
+      <SidebarOption text="Profile" Icon={PermIdentityIcon} />
+      <SidebarOption text="More" Icon={MoreHorizIcon} />
+      {/* SidebarOption */}
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>Tweet</Button>
+      {/* Button -> Tweet */}
     </div>
   )
 }
