a4bcdd9ff657915c460eec86b43ae00abec361a9
latemiq <123744356+latemiq@users.noreply.github.com>
2026-02-04 09:49:59 +0100

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_TWITTER_API_KEY,
   authDomain: process.env.REACT_APP_TWITTER_AUTH_DOMAIN,
-  databaseURL: "https://twitter-clone-5fecf-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "twitter-clone-5fecf",
-  storageBucket: "twitter-clone-5fecf.firebasestorage.app",
-  messagingSenderId: "863373412960",
-  appId: "1:863373412960:web:23e233bf7ea025c0ed5105",
-  measurementId: "G-B0XV1RES8V"
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
