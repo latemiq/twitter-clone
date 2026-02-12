3e17635b3ce54bf5b3c5243ee3bfa54a202373f5
latemiq <123744356+latemiq@users.noreply.github.com>
2026-02-12 19:12:26 +0100

    update

--- src/features/tweets/components/Feed.js
+++ src/features/tweets/components/Feed.js
@@ -21,12 +21,12 @@ function Feed() {
   return (
     <div className='feed border-r-[1px] border-twitter-background min-w-fit overflow-y-scroll'>
       <div className='feed__header sticky top-0 bg-white z-100 border-[1px] border-twitter-background p-[15px 20px]'>
-        <div className='feed__headerTitle flex items-center justify-center'>
+        <div className='feed__headerTitle flex items-center justify-center gap-[30%]'>
           <div className='flex items-center gap-[10px]'>
-            <h2 className='text-[20px] font-bold-800'>For you</h2>
+            <h2 className='text-[20px] font-[calibri] font-extrabold'>For you</h2>
           </div>
-          <div>
-            <h2 className='text-[20px] font-bold-800'>Following</h2>
+          <div className='flex items-center gap-[10px]'>
+            <h2 className='text-[20px] font-[calibri] font-extrabold'>Following</h2>
           </div>
         </div>
       </div>
